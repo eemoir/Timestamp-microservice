83a81a316ff36a1208b4aaa72d796fcd84bb30da
eemoir <33586203+eemoir@users.noreply.github.com>
2018-05-25 17:39:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-// server.js
-// where your node app starts
-
 // init project
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
